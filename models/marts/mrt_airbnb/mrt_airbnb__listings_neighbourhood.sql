ffd991d35a21356f571ec86da913865d21be9410
Damien Azzopardi <36568661+DamienAzzopardi@users.noreply.github.com>
2026-02-12 09:47:49 +0100

    Revert "Removing room type dimension from mrt-airbnb__listings_neighbourhood"

--- models/marts/mrt_airbnb/mrt_airbnb__listings_neighbourhood.sql
+++ models/marts/mrt_airbnb/mrt_airbnb__listings_neighbourhood.sql
@@ -10,11 +10,13 @@ neighbourhood_prices as (
 
     select
         neighbourhood_cleansed,
+        room_type,
         count(id) as total_listings,
         round(avg(price), 2) as avg_price
     from {{ ref('stg_airbnb__listings') }}
     group by
-        neighbourhood_cleansed
+        neighbourhood_cleansed,
+        room_type
 
 ),
 
@@ -22,6 +24,7 @@ final as (
 
     select
         neighbourhood_cleansed,
+        room_type,
         total_listings,
         avg_price
     from neighbourhood_prices
